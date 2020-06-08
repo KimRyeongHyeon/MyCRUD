bc7b3c8a5b60ded252336479ed844009731dbcda
KimRyeongHyeon <52704419+KimRyeongHyeon@users.noreply.github.com>
2020-06-08 12:30:44 +0900

    Android + PHP + MySQL CRUD

---
+++ app/src/main/java/com/myandroid/mycrud/AddDataActivity.java
@@ -0,0 +1,112 @@
+package com.myandroid.mycrud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AddDataActivity extends AppCompatActivity {
+
+    EditText txtName, txtEmail, txtContact, txtAddress;
+    Button btn_insert;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_data);
+
+        txtName = findViewById(R.id.edtName);
+        txtEmail = findViewById(R.id.edtEmail);
+        txtContact = findViewById(R.id.edtContact);
+        txtAddress = findViewById(R.id.edtAddress);
+        btn_insert = findViewById(R.id.btnInsert);
+
+        btn_insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                insertData();
+            }
+        });
+    }
+
+    private void insertData() {
+        final String name = txtName.getText().toString().trim();
+        final String email = txtEmail.getText().toString().trim();
+        final String contact = txtContact.getText().toString().trim();
+        final String address = txtAddress.getText().toString().trim();
+
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading...");
+
+        if (name.isEmpty()) {
+            Toast.makeText(this, "Enter Name", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (email.isEmpty()) {
+            Toast.makeText(this, "Enter Email", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (contact.isEmpty()) {
+            Toast.makeText(this, "Enter Contact", Toast.LENGTH_SHORT).show();
+            return;
+        } else if (address.isEmpty()) {
+            Toast.makeText(this, "Enter Address", Toast.LENGTH_SHORT).show();
+            return;
+        } else {
+            progressDialog.show();
+            StringRequest request = new StringRequest(Request.Method.POST, "https://soulstring94.cafe24.com/insert.php",
+                    new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            if(response.equalsIgnoreCase("Data Inserted")) {
+                                Toast.makeText(AddDataActivity.this, "Data Inserted", Toast.LENGTH_SHORT).show();
+                                progressDialog.dismiss();
+                            } else {
+                                Toast.makeText(AddDataActivity.this, response, Toast.LENGTH_SHORT).show();
+                                progressDialog.dismiss();
+                            }
+                        }
+                    }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Toast.makeText(AddDataActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+                    progressDialog.dismiss();
+                }
+            }
+
+            ) {
+                @Override
+                protected Map<String, String> getParams() {
+                    Map<String, String> params = new HashMap<String, String>();
+                    params.put("name", name);
+                    params.put("email", email);
+                    params.put("contact", contact);
+                    params.put("address", address);
+
+                    return params;
+                }
+            };
+
+            RequestQueue requestQueue = Volley.newRequestQueue(AddDataActivity.this);
+            requestQueue.add(request);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finish();
+    }
+}
